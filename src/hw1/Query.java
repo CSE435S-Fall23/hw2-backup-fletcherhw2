2d2ee78fee29a0478cd0a88bd3deb867b8a59961
Ben Fletcher <benfletcher@wustl.edu>
2023-10-16 01:54:30 -0500

    dawdwadwada

--- src/hw1/Query.java
+++ src/hw1/Query.java
@@ -4,6 +4,8 @@
 package hw1;
 
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.List;
 
 import net.sf.jsqlparser.JSQLParserException;
@@ -21,6 +23,7 @@ import net.sf.jsqlparser.statement.select.Join;
 import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.select.Select;
 import net.sf.jsqlparser.statement.select.SelectBody;
+import net.sf.jsqlparser.statement.select.SelectExpressionItem;
 import net.sf.jsqlparser.statement.select.SelectItem;
 
 public class Query {
@@ -43,7 +46,7 @@ public class Query {
 		PlainSelect sb = (PlainSelect)selectStatement.getSelectBody();
 		//sb.getSelectItems();
 		
-		
+		boolean groupByFlag = false;
 		
 		//get the heap file that is listed in the FROM clause
 		String heapfileName = sb.getFromItem().toString();
@@ -67,7 +70,7 @@ public class Query {
 		List<Join> listJoins = sb.getJoins();
 		ArrayList<String> projectedColumns = new ArrayList<String>();
 		
-		
+	/*	
 		//handle joins first, not efficient but makes it easier
 		for(Join j: listJoins) {
 			
@@ -99,53 +102,109 @@ public class Query {
 			
 			starterRelation = starterRelation.join(starterRelationRight, fieldL, fieldR);
 		}
+	*/
+		
+		//get where statements
+		if(sb.getWhere()!= null) {
+			WhereExpressionVisitor we = new WhereExpressionVisitor();
+			Expression whereE = sb.getWhere();
+			whereE.accept(we);	
+			
+			String lwhere = we.getLeft();
+			Field rwhere = we.getRight();
+			RelationalOperator opR = we.getOp();
+			
+			//call select so only the correct rows are in the relation
+			starterRelation.select(initDesc.nameToId(lwhere), opR, rwhere);
+		}
+
+		//get groupBy
+		if(sb.getGroupByColumnReferences()!= null) {
+			groupByFlag = true;
+			
+		}
 		
 		
+		//needed because visitor overwrites renames
+		//will occur at end of code after other operations have finished
+		ArrayList<Integer> renamedFields = new ArrayList<Integer>();
+		ArrayList<String> renamedNames = new ArrayList<String>();
 		
 		//loop through all columns listed after the select
 		for(SelectItem si:listSelectColumns) {
 			
 			ColumnVisitor cv = new ColumnVisitor();
-			WhereExpressionVisitor we = new WhereExpressionVisitor();
+			
 			//grab select columns
 			si.accept(cv);
-			//grab where clause details
-			si.accept(we);
-			
-			
+	
 			visList.add(cv);
-			whereVisList.add(we);
+			
+			//make sure column can be renamed later but doesnt lose its value
+			SelectExpressionItem sic = (SelectExpressionItem) si;
+			
+			if(sic.getAlias()!=null) {
+				renamedNames.add(sic.getAlias().getName());
+			}
 			
 		}
-		
-		
-		//select the rows we want
-		for(WhereExpressionVisitor whereV:whereVisList) {
-			
-		}
-		
 		
 		
 		//project the final columns that are after the select clause
 		
+		Relation projectedRelation = starterRelation;
+		/*
+		ArrayList<Tuple> Al = new ArrayList<Tuple>();
+		Type[] types = new Type[0];
+		String[] names = new String[0];
+		TupleDesc tdnew = new TupleDesc(types,names);
+		Relation finalRelation = new Relation(Al,tdnew);
+		*/
 		for(ColumnVisitor columnv: visList) {
 			
-			//handle select all
-			if(columnv.getColumn()=="*") {
-
+			//handle select all, return all columns
+			if(columnv.getColumn() == "*") {
+				return starterRelation;
 			}
 			//if column is aggregate
 			if(columnv.isAggregate()) {
-				AggregateOperator columnAGGREGATE = columnv.getOp();
-				starterRelation.aggregate(columnAGGREGATE, false);
+				
+				if(groupByFlag) {
+					AggregateOperator columnAGGREGATE = columnv.getOp();
+					starterRelation.aggregate(columnAGGREGATE, true);					
+				}
+				else {
+					AggregateOperator columnAGGREGATE = columnv.getOp();
+					starterRelation.aggregate(columnAGGREGATE, false);
+				}
+				
+				//finalRelation.joinProjectionsTogether(starterRelation);
+
 			}
 			//column is just a normal, non * column
 			else 
 			{
-				//projectedColumns
+				/*
+				ArrayList<Integer> fieldsAL = new ArrayList<Integer>();
+				fieldsAL.add(hf.getTupleDesc().nameToId(columnv.getColumn()));
+				
+				projectedRelation = starterRelation.project(fieldsAL);
+				
+				
+				finalRelation.joinProjectionsTogether(projectedRelation);
+				
+				*/
 			}
 			
 		}
+		
+		
+		//rename after everything important has been done
+		for(String name: renamedNames) {
+			renamedFields.add(hf.getTupleDesc().nameToId(name));
+		}
+		
+		starterRelation.rename(renamedFields, renamedNames);
 		
 		
 		
--- src/test/YourUnitTests.java
+++ src/test/YourUnitTests.java
@@ -2,14 +2,54 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import hw1.Catalog;
+import hw1.Database;
+import hw1.Query;
+import hw1.Relation;
 
 
 public class YourUnitTests{
 	
+	private Catalog c;
+	
+	@Before
+	public void setup() {
+		try {
+			Files.copy(new File("testfiles/test.dat.bak").toPath(), new File("testfiles/test.dat").toPath(), StandardCopyOption.REPLACE_EXISTING);
+			Files.copy(new File("testfiles/A.dat.bak").toPath(), new File("testfiles/A.dat").toPath(), StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			System.out.println("unable to copy files");
+			e.printStackTrace();
+		}
+		
+		c = Database.getCatalog();
+		c.loadSchema("testfiles/test.txt");
+		
+		c = Database.getCatalog();
+		c.loadSchema("testfiles/A.txt");
+	}
+	
 	@Test
-	public void test1() {
-		assertEquals("dqogqogfqp",true,true);	
+	public void QueryASTEST() {
+		Query q = new Query("SELECT a2 AS secondColumn FROM A");
+		Relation r = q.execute();
+		
+		assertTrue(r.getDesc().getFieldName(0).equals("secondColumn"));
+		
+		
+		//same tests from testProject in other unit tests
+		//to double check correctness
+		assertTrue(r.getDesc().getSize() == 4);
+		assertTrue(r.getTuples().size() == 8);
+
 	}
 	
 }
